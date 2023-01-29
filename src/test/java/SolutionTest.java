6e338019b120e1466343f86d3a80a0d58a981fec
pockocmoc <pockocmoc@inbox.ru>
2023-01-29 18:10:31 +0300

    SolutionTest update

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -22,9 +22,5 @@ class SolutionTest {
         assertEquals(70, Solution.countPaths(0, 0, 5, 5));
     }
 
-    @org.junit.jupiter.api.Test
-    void countPathsFive() {
-        assertEquals(70, Solution.countPaths(0, 0, 5, 5));
-    }
 
 }
