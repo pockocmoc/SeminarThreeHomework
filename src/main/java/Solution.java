4150467ea27b21fa1f2d54cd2884606d8098d37a
pockocmoc <pockocmoc@inbox.ru>
2023-01-29 14:33:10 +0300

    Change N to n.

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,12 +1,12 @@
 public class Solution {
-    static int countPaths(int x, int y, int N) {
-        if (x == N - 1 && y == N - 1) {
+    static int countPaths(int x, int y, int n) {
+        if (x == n - 1 && y == n - 1) {
             return 1;
         }
-        if (x > N - 1 || y > N - 1) {
+        if (x > n - 1 || y > n - 1) {
             return 0;
         }
-        return countPaths(x + 1, y, N) + countPaths(x, y + 1, N);
+        return countPaths(x + 1, y, n) + countPaths(x, y + 1, n);
     }
 
 }
